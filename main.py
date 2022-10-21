663c99e13f0902f93aa5c7c61badf1d4b532fcfc
lgz <sagiri.wo@gmail.com>
2022-10-21 14:14:45 +0800

    添加自动预约功能

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from datetime import date, timedelta
 import requests
 import smtplib
 from email.mime.text import MIMEText
-
+import yaml
+from json import dumps
 
 def get_next_next_days(offset):
     # 今天日期
@@ -14,69 +15,7 @@ def get_next_next_days(offset):
     next4 = (4 - weekday) if weekday <= 4 else (11 - weekday) + offset
     return ['', '', '', (today + timedelta(next3)).strftime('%Y-%m-%d'), (today + timedelta(next4)).strftime('%Y-%m-%d')]
 
-day_name = ['', '', '', '三', '四']
-
-# 设置服务器所需信息
-# 163邮箱服务器地址，其他邮箱也可以，但是QQ邮箱有个额外的验证
-mail_host = 'smtp.163.com'
-# 163用户名
-mail_user = 'lgz1104738025'
-# 密码(部分邮箱为授权码)
-mail_pass = 'UGTACYYAULPISHXG'
-# 邮件发送方邮箱地址
-sender = 'lgz1104738025@163.com'
-# 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-receivers = ['1104738025@qq.com', '1135664656@qq.com']
-
-
-# url
-baseurl = 'https://apixy.xingying2018.com/api/WebChatAppointment/GetAppointmentList'
-
-# headers
-headers = {
-    "Origin": "https://weixinzj.xingying2018.com"
-}
-
-# params
-typeid = 2  # 2是普通拍摄，1是精品
-# offset 控制下周还是下下周
-offset = [0, 7]
-
-
-content = ''
-# 发送请求
-for o in offset:
-    dt = get_next_next_days(o)  # 获取下下一个周三和周四
-
-    # 下一个周三的url
-    url3 = baseurl + '?typeid=' + str(typeid) + '&dt=' + dt[3]
-    # 下一个周四的url
-    url4 = baseurl + '?typeid=' + str(typeid) + '&dt=' + dt[4]
-
-    url = ['', '', '', url3, url4]
-    for i in range(3, 5):
-        data = requests.get(url[i], headers=headers).json()
-        if data['statusCode'] == 201:
-            print('周' + day_name[i] + dt[i] + ':' + data['message'])
-        elif data['statusCode'] == 200:
-            cnt = 0
-            for item in data['data']['appointmentdata']:
-                appointCount = item['appointCount']
-                appointNum = item['appointNum']
-                dateSlot = item['dateSlot']
-                if appointCount < appointNum:
-                    cnt += 1
-                    content += '周' + day_name[i] + '(' + dt[i] + ')' + '时间段：' + dateSlot + '剩余' + str(
-                        appointNum - appointCount) + '名额(' + str(appointCount) + '/' + str(appointNum) + ')\n'
-            if 0 == cnt:
-                print('周' + day_name[i] + dt[i] + ':' + '全都已满')
-        else:
-            print('出现未知错误')
-
-# 发送邮件
-if content != '':
-    # 设置email信息
-    # 邮件内容设置
+def send(content, receivers, mail_host, port, mail_user, mail_pass):
     message = MIMEText(content, 'plain', 'utf-8')
     # 邮件主题
     message['Subject'] = '拍照预约有啦！'
@@ -87,12 +26,12 @@ if content != '':
     # 登录并发送邮件
     try:
         # 连接到服务器
-        smtpObj = smtplib.SMTP_SSL(mail_host, 465)
+        smtpObj = smtplib.SMTP_SSL(mail_host, port)
         # 登录到服务器
         smtpObj.login(mail_user, mail_pass)
         # 发送
         smtpObj.sendmail(
-        sender, receivers, message.as_string())
+            sender, receivers, message.as_string())
         # 退出
         smtpObj.quit()
         print('邮件发送成功')
@@ -100,3 +39,88 @@ if content != '':
         # 打印错误
         print('error', e)
 
+day_name = ['', '', '', '三', '四']
+
+# 查询url
+baseurl = 'https://apixy.xingying2018.com/api/WebChatAppointment/GetAppointmentList'
+# 预约url
+addurl = 'https://apixy.xingying2018.com/api/WebChatAppointment/AddAppointment'
+
+# headers
+headers = {
+    "Origin": "https://weixinzj.xingying2018.com",
+    "Content-Type": "application/json"
+}
+
+# params
+typeid = 2  # 2是普通拍摄，1是精品
+# offset 控制下周还是下下周
+offset = [0, 7]
+# 邮件内容
+content = ''
+cfg = dict()
+with open('config.yml', encoding='utf-8', mode='r') as f:
+    # 读取配置文件
+    cfg = yaml.safe_load(f)
+    mail_host = cfg['mail']['sender']['host']
+    mail_user = cfg['mail']['sender']['user']
+    mail_pass = cfg['mail']['sender']['password']
+    sender = cfg['mail']['sender']['address']
+    port = cfg['mail']['sender']['port']
+    receivers = cfg['mail']['receivers']
+    plan = cfg['plan']
+    # 发送请求, offset控制拿哪一周的数据
+    for o in offset:
+        dt = get_next_next_days(o)  # 获取周三和周四的日期
+
+        # 周三的url
+        url3 = baseurl + '?typeid=' + str(typeid) + '&dt=' + dt[3]
+        # 周四的url
+        url4 = baseurl + '?typeid=' + str(typeid) + '&dt=' + dt[4]
+
+        url = ['', '', '', url3, url4]
+        for i in range(3, 5):
+            data = requests.get(url[i], headers=headers).json()
+            if data['statusCode'] == 201:
+                print('周' + day_name[i] + dt[i] + ':' + data['message'])
+            elif data['statusCode'] == 200:
+                cnt = 0
+                for item in data['data']['appointmentdata']:
+                    id = item['id']
+                    appointCount = item['appointCount']
+                    appointNum = item['appointNum']
+                    dateSlot = item['dateSlot']
+                    # 还有剩余人数
+                    if appointCount < appointNum:
+                        cnt += 1
+                        content += '周' + day_name[i] + '(' + dt[i] + ')' + '时间段：' + dateSlot + '剩余' + str(
+                            appointNum - appointCount) + '名额(' + str(appointCount) + '/' + str(appointNum) + ')\n'
+                        # 在配置文件中寻找想要预约这个事件段的人
+                        for p in plan:
+                            # 存在并且还没预约过
+                            if dt[i] == p['date'] and dateSlot == p['time'] and p['success'] == False:
+                                json_data = {
+                                    "ID": id,
+                                    "OpenID": plan[0]['openid'],
+                                    "DateSlot": plan[0]['time'],
+                                    "AppointDate": plan[0]['date']
+                                }
+                                res_data = requests.post(addurl, data=dumps(json_data), headers=headers).json()
+                                if res_data['statusCode'] == 200:
+                                    send('预约成功', [p['mail']], mail_host, port, mail_user, mail_pass)
+                                    # 稍后会写入配置文件
+                                    p['success'] = True
+                                    print('预约成功邮件 发送成功')
+
+                if 0 == cnt:
+                    print('周' + day_name[i] + dt[i] + ':' + '全都已满')
+            else:
+                print('出现未知错误')
+
+    # 发送邮件
+    if content != '':
+        send(content, receivers, mail_host, port, mail_user, mail_pass)
+
+
+with open('config.yml', encoding='utf-8', mode='w') as f:
+    yaml.dump(cfg, f)
